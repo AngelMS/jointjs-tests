4d62b7f070557a2deb885a827f3921c39086e30f
AngelMS <Angel.m.Some@gmail.com>
2018-04-13 13:56:45 +0200

    new examples added

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    // Diagrams consist of elements connected with links. A diagram in JointJS is represented by a model 
-    // joint.dia.Graph. This model then collects cells (a term representing both elements and links). 
-    // A cell could therefore be either an element (joint.dia.Element or its inheritants) or a link 
+    // Diagrams consist of elements connected with links. A diagram in JointJS is represented by a model
+    // joint.dia.Graph. This model then collects cells (a term representing both elements and links).
+    // A cell could therefore be either an element (joint.dia.Element or its inheritants) or a link
     // (joint.dia.Link). In JointJS (starting from version 0.6), you manipulate models, not views.
-    
-    
-    // This is the model to be used defined without coupling with the views - Models represents a diagrams
+
+
+    // This is the model to be used without coupling with the views - Models represents a diagrams
     var graph = new joint.dia.Graph;
-    
+
     // This is a view using the model without defining its implementation
     var paper = new joint.dia.Paper({
         el: $('#myholder'),
@@ -16,7 +16,7 @@ $(document).ready(function() {
         model: graph,
         gridSize: 1
     });
-    
+
     // This is another view using the same model than the above one. Models can have multiple views
     // "model has views" = "is used by multiple views" - (the former is the documentation notation)
     var paperSmall = new joint.dia.Paper({
@@ -28,43 +28,43 @@ $(document).ready(function() {
     });
     paperSmall.scale(.5);
     paperSmall.$el.css('pointer-events', 'none');
-    
+
     // Elements are considered cells
     var rect = new joint.shapes.basic.Rect({
         position: { x: 100, y: 30 },
         size: { width: 100, height: 30 },
         attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
     });
-    
+
     // Again, another cell (element)
     var rect2 = rect.clone();
     rect2.translate(300);
-    
+
     // Links are considered cells as well
     var link = new joint.dia.Link({
         source: { id: rect.id },
         target: { id: rect2.id },
         smooth: true
     });
-    
+
     // properties can be set at construction time or afterwards using the .set method
     // ===================================== (this snippet is the same than the above)
     // var link = new joint.dia.Link({
     //     target: { id: rect2.id }
     // });
-    // 
+    //
     // link.set("smooth", true)
     // link.set("source", { id: rect.id })
     // =====================================
-    
+
     // The model collects cells (a term representing both elements and links)
     graph.addCells([rect, rect2, link]);
-    
+
     // Logs all events for all the elements
     // graph.on('all', function(eventName, cell) {
     //     console.log(arguments);
     // });
-    
+
     // Logs a particular element when it change its position
     rect.on('change:position', function(element) {
         console.log(element.id, ':', element.get('position'));
